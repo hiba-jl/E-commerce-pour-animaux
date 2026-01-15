c1eac3971c98dac945138a62dc73b425c827a581
hiba-jl <hibajl5612@gmail.com>
2026-01-15 23:01:05 +0100

    Update start-backend.bat

--- start-backend.bat
+++ start-backend.bat
@@ -1,18 +1,1 @@
-@echo off
-echo ====================================
-echo    Lancement du Backend
-echo ====================================
-cd backend
-if not exist node_modules (
-    echo Installation des dependances...
-    call npm install
-)
-echo.
-echo Demarrage du serveur backend...
-echo Le serveur sera accessible sur http://localhost:5000
-echo.
-call npm run dev
-pause
-
-
 
