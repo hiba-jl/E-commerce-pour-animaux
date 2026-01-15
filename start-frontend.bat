c24106a2b13acc7a8bf8025b20485c6fd22be888
hiba-jl <hibajl5612@gmail.com>
2026-01-15 23:01:18 +0100

    Update start-frontend.bat

--- start-frontend.bat
+++ start-frontend.bat
@@ -1,18 +1,1 @@
-@echo off
-echo ====================================
-echo    Lancement du Frontend
-echo ====================================
-cd frontend
-if not exist node_modules (
-    echo Installation des dependances...
-    call npm install
-)
-echo.
-echo Demarrage du serveur frontend...
-echo Le navigateur va s'ouvrir automatiquement sur http://localhost:3000
-echo.
-call npm start
-pause
-
-
 
